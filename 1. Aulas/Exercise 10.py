7a4a9ee5e14927b5c81b039d980edd635f6fd1f0
vincegarcia88 <126945619+vincegarcia88@users.noreply.github.com>
2024-06-25 12:32:32 +0100

    Delete 1. Aulas directory

--- 1. Aulas/Exercise 10.py
+++
@@ -1,7 +0,0 @@
-print("--- Calculadora ---")
-print("[ 1 ] – Tabuada")
-print ("[ 2 ] – Calculadora")
-print ("[ 3 ] – Fatorial")
-print ("[ 4 ] – Números primos")
-opcao= input ()
-print ("Escolheste a opção", opção
--- 1. Aulas/Exercise 11.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("First number "))
-num2 = int (input("Second number "))
-
-soma = num1 + num2
-subtracao = num1 - num2
-
-print(soma)
-print(subtracao)
