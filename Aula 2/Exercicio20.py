c2bd3ab7278568120a553f1796f6908dd2ee2b8d
vincegarcia88 <126945619+vincegarcia88@users.noreply.github.com>
2024-06-26 11:29:13 +0000

    Add files via upload

---
+++ Aula 2/Exercicio20.py
@@ -0,0 +1,21 @@
+#Crie um programa que leia o nome
+#completo de uma pessoa e mostre:
+#✓ O nome com todas as letras maiúsculas;
+#✓ O nome com todas as letras minúsculas;
+#✓ Quantidade de letras sem espaços;
+#✓ Quantas letras tem o primeiro nome.
+
+#Todas as letras maiúsculas
+nome = input("Insira seu nome completo: ")
+print (nome.upper())
+
+#Todas as letras minúsculas
+print (nome.lower())
+
+#Quantidade de letras sem espaços
+nome.split [nome]
+print (nome.count [1])
+
+#Quantas letras tem o primeiro nome
+
+
---
+++ Aula 2/Exercicio21.py
@@ -0,0 +1,16 @@
+#Crie um programa que leia uma
+#frase e mostre:
+#✓ Quantas vezes aparece a letra “A”;
+#✓ Em que posição aparece a primeira vez;
+#✓ Em que posição aparece a última vez.
+
+name = input ("Escreva seu nome: ")
+
+#Quantas vezes aparece a letra A
+print (name.count("a"))
+
+#Em que posição aparece a primeira vez
+
+
+
+
