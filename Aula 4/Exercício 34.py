9ef82e89f56d80954d531b60747ea6999f126646
vincegarcia88 <126945619+vincegarcia88@users.noreply.github.com>
2024-07-08 11:30:35 +0100

    Aula 4 directory

--- Aula 4/Exercício 34.py
+++
@@ -1,5 +0,0 @@
-#Faça um programa que leia 10 números e
-#conte quantos deles são pares.
-
-for c in range (2,11, 2):
-    print (c)
--- Aula 4/Exercício 36.py
+++
@@ -1,8 +0,0 @@
-#Faça um programa que mostre a tabuada
-#de um número introduzido pelo
-#utilizador.
-
-num1 = int(input("Insira um número: "))
-
-for c in range (1,11):
-    print (f"{num1}x{c} = {num1 * c}")
