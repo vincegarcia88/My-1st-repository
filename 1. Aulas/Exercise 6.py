dac1f24aac9062c4176f148ab404390d3ca465d4
Vince Garcia <126945619+vincegarcia88@users.noreply.github.com>
2024-07-15 19:14:26 +0000

    Delete 1. Aulas directory

--- 1. Aulas/Exercise 6.py
+++
@@ -1,7 +0,0 @@
-print("Crie o seguinte menu:\n")
-print("--- Calculadora ---\n")
-print("[ 1 ] – Tabuada\n")
-print("[ 2 ] – Calculadora\n")
-print("[ 3 ] – Fatorial\n")
-print("[ 4 ] – Números primos\n")
-
--- 1. Aulas/Exercise 7.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("Digite um número "))
-num2 = int(input("Digite outro número "))
-
-multiplicacao = num1 * num2
-divisao = num1 / num2
-
-print ("O resultado da multiplicação é ")(multiplicacao)
-print ("O resultado da divisão é ")(divisao)
