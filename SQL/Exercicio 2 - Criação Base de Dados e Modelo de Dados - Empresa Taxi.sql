697d01c0864e79af2a3beef1e39b2bfae794e5df
Vince Garcia <126945619+vincegarcia88@users.noreply.github.com>
2024-07-25 18:35:59 +0000

    Update Exercicio 2 - Criação Base de Dados e Modelo de Dados - Empresa Taxi.sql

--- SQL/Exercicio 2 - Criação Base de Dados e Modelo de Dados - Empresa Taxi.sql
+++ SQL/Exercicio 2 - Criação Base de Dados e Modelo de Dados - Empresa Taxi.sql
@@ -1,60 +1,70 @@
--- Exercício 1 - Criação de base de Dados e Modelo de Dados - Empresa de Táxis
+-- Exercício Prático 4 - Modelação Base de Dados - Empresa de Táxis
+
 -- Considere o seguinte problema:
 
 -- Uma empresa de táxis pretende implementar uma base de dados com a seguinte informação:
 
--- A empresa tem vários táxis. Cada táxi tem, para além da matrícula, um nome, a marca, o número de lugares,
--- o ano de fabrico, a cor e se tem GPS.
--- Existem várias marcas. Para cada marca é necessário guardar o seu id e nome. Na empresa trabalham vários condutores de táxi.
--- Para cada condutor é necessário guardar o seu nome, morada e data de nascimento, além do número da sua carta de condução.
--- Existem condutores que pela sua idade estão apenas em funções administrativas dentro da empresa, estando impossibilitados de conduzir.
--- Cada táxi faz várias viagens. Cada viagem deve ter a indicação do código, da data e hora em que se efetua, do local de partida
+-- A empresa tem vários táxis. Cada táxi tem, para além da matrícula, um nome, a marca, o número de lugares, o ano de fabrico, a cor e se tem GPS. 
+-- Existem várias marcas. Para cada marca é necessário guardar o seu ID e nome. 
+
+-- Na empresa trabalham vários condutores de táxi. Para cada condutor é necessário guardar o seu nome, morada e data de nascimento, além do número
+-- da sua carta de condução. 
+
+-- Existem condutores que pela sua idade estão apenas em funções administrativas dentro da empresa, estando impossibilitados
+-- de conduzir. Cada táxi faz várias viagens. 
+
+-- Cada viagem deve ter a indicação do código, da data e da hora em que se efetua, do local de partida
 -- e do local de destino, bem como do número de pessoas que transportou e o preço.
--- Cada viagem de um dado táxi é feita por um condutor apenas.
 
--- 1. Criação do banco de dados
-CREATE DATABASE IF NOT EXISTS empresa_taxi;
+-- Cada viagem de um dado táxi é feita por um condutor apenas. 
 
-USE empresa_taxi;
+-- 1. Faça o modelo ER para o problema apresentado
+-- 2. Faça o diagrama ER do modelo criado na pergunta anterior
+-- 3. Converta o modelo ER para o modelo racional
+-- 4. Crie o diagrama de Modelo relacional
 
--- 2. Tabela de Marcas
-CREATE TABLE IF NOT EXISTS marcas (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    Nome_marca VARCHAR(100) NOT NULL
+-- 0. Criação do banco de dados
+CREATE DATABASE IF NOT EXISTS Empresa_taxi;
+USE Empresa_taxi;
+
+-- 1. Tabela de marcas
+CREATE TABLE IF NOT EXISTS marca(
+	Marca_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Marca_nome VARCHAR (100) NOT NULL
 );
 
--- 3. Tabela de Táxis
-CREATE TABLE IF NOT EXISTS taxi(
-	Taxi_Matricula VARCHAR (100) AUTO_INCREMENT PRIMARY KEY,
-    Nome_carro VARCHAR (100) NOT NULL,
-    Marca_ID INT NOT NULL,
-    Número_lugares INT NOT NULL,
-    Ano_Fabrico YEAR NOT NULL,
-    Cor VARCHAR (100),
-    GPS BOOLEAN,
-    CONSTRAINT FOREIGN KEY (Marca_ID) REFERENCES marcas(id)
+-- 2. Tabela de Taxis
+CREATE TABLE IF NOT EXISTS Taxi(
+	Taxi_matricula VARCHAR (100) PRIMARY KEY,
+    Taxi_marca INT NOT NULL,
+    Taxi_nome VARCHAR (100) NOT NULL,
+    numero_lugares INT NOT NULL,
+    ano_fabrico YEAR NOT NULL,
+    Cor_Taxi VARCHAR (100) NOT NULL,
+    GPS BOOLEAN NOT NULL,
+    CONSTRAINT fk_Taxi_marca FOREIGN KEY (Taxi_marca) REFERENCES marca (Marca_ID)
 );
 
--- 4. Condutor
-CREATE TABLE IF NOT EXISTS Condutor_Info(
-	Num_Carta_Conducao PRIMARY KEY INT NOT NULL,
-	Nome_condutor VARCHAR (100) NOT NULL,
-	Morada_condutor VARCHAR (100) NOT NULL,
-	Data_Nascimento DATE NOT NULL,
-    Ativo BOOLEAN NOT NULL,
-    CONSTRAINT FOREIGN KEY (nome_condutor) REFERENCES taxi(condutor)
+-- 3. Tabela de condutores
+CREATE TABLE IF NOT EXISTS Condutores(
+	Condutor_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Nome_condutor VARCHAR (50) NOT NULL,
+    Condutor_Morada VARCHAR (100) NOT NULL,
+    Data_nascimento DATE NOT NULL,
+    Ativo BOOLEAN NOT NULL
 );
 
--- 5. Informação da Viagem
-CREATE TABLE IF NOT EXISTS Viagem_taxi(
-	Codigo_viagem INT NOT NULL AUTO_INCREMENT,
-    data_viagem DATE NOT NULL,
-    hora_viagem_ida DATETIME,
-    hora_viagem_volta DATETIME,
-    numero_passageiros INT NOT NULL,
-    preço_viagem DECIMAL (15,2)
+-- 4. Tabela de viagens
+CREATE TABLE IF NOT EXISTS Viagens(
+	Codigo_viagem INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Taxi_matricula VARCHAR (100) NOT NULL,
+    Condutor_ID INT NOT NULL,
+    Data_viagem DATE NOT NULL,
+    Hora_viagem DATETIME NOT NULL,
+    Local_partida VARCHAR (100) NOT NULL,
+    Local_chegada VARCHAR (100) NOT NULL,
+    Numero_pessoas INT NOT NULL,
+    Preco_viagem DECIMAL (10,2) NOT NULL,
+    CONSTRAINT fk_Viagens_Taxi FOREIGN KEY Taxi_matricula REFERENCES taxi (Taxi_matricula),
+    CONSTRAINT fk_Viagens_Condutores FOREIGN KEY Condutor_ID REFERENCES Condutores (Condutor_ID)
 );
-
-
-	
-
