0f5998d281fcbd1195f40498704e19e6fc351cf4
Vince Garcia <126945619+vincegarcia88@users.noreply.github.com>
2024-08-12 11:01:47 +0000

    Add files via upload

---
+++ Aula 8 - Array Multidimensional/Exercicio 62.py
@@ -0,0 +1,6 @@
+'''Melhore o exercício 61, exibindo no final.
+
+a) A soma de todos os valores pares.
+b) A soma dos valores da segunda coluna.
+c) O maior valor da terceira linha.'''
+
---
+++ Aula 8 - Array Multidimensional/Exercicio 63.py
@@ -0,0 +1,5 @@
+'''Cria um programa que crie palpites para o Euromilhões.
+O programa deve perguntar quantas chaves serão geradas
+e deve sortear aleatoriamente 5 números de 1 a 50 [sem repetir] e 2 estrelas de
+1 a 12 [sem repetir]. Cada sorteio deve ser guardado numa lista composta.'''
+
